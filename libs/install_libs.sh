8c5d1ae33d19592fbeca889437dd18859f2f3fdc
AnonymousAnalyst <>
2021-03-01 16:12:09 +0100

    add magpie

--- libs/install_libs.sh
+++ libs/install_libs.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 LPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 mvn install:install-file -Dfile=$LPATH/AQL-System-2.0.0-SNAPSHOT.jar -DgroupId=de.foellix -DartifactId=AQL-System -Dversion=2.0.0-SNAPSHOT -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -Dfile=$LPATH/magpiebridge-0.0.6.jar -DgroupId=magpiebridge -DartifactId=magpiebridge -Dversion=0.0.6 -Dpackaging=jar -DgeneratePom=true
